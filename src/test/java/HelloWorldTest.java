dd2c6dd5f9f9bf5d8dfa975ff10e7a9c0653283e
Valeria Reshetina <dmitry.potapov@icloud.com>
2023-04-24 20:31:04 +0300

    testHelloForEx3 and testForEx4 were created

--- src/test/java/HelloWorldTest.java
+++ src/test/java/HelloWorldTest.java
@@ -1,9 +1,27 @@
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
 import org.junit.jupiter.api.Test;
 
 public class HelloWorldTest {
 
     @Test
     public void testHelloWorld() {
-        System.out.println("Hello world");
+        Response response = RestAssured
+                .get("https://playground.learnqa.ru/api/hello")
+                .andReturn();
+        response.prettyPrint();
+    }
+
+    @Test
+    public void testHelloForEx3() {
+        System.out.println("Hello from Valeria");
+    }
+
+    @Test
+    public void testForEx4() {
+        Response response = RestAssured
+                .get("https://playground.learnqa.ru/api/get_text")
+                .andReturn();
+        response.prettyPrint();
     }
 }
