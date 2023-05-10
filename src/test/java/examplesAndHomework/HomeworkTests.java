ff51382b137c2530e4740b56609ae0eede66c251
Valeria Reshetina <dmitry.potapov@icloud.com>
2023-05-10 10:21:56 +0300

    homework_11_testRequestForCookieMethod

--- src/test/java/examplesAndHomework/HomeworkTests.java
+++ src/test/java/examplesAndHomework/HomeworkTests.java
@@ -175,4 +175,15 @@ public class HomeworkTests {
         String myString = "Test for checking length of string";
         assertThat(myString, hasLength(34));
     }
+
+    @Test
+    public void testRequestForCookieMethodForEx11() {
+        Response response = RestAssured
+                .given()
+                .get("https://playground.learnqa.ru/api/homework_cookie")
+                .andReturn();
+        Map<String, String> cookies = response.getCookies();
+
+        assertEquals("hw_value", cookies.get("HomeWork"));
+    }
 }
